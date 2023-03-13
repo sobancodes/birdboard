781b04279f5ad7200793a8f9cbb5242c8cd106e3
Soban Farooq <sobanfarooq3@gmail.com>
2023-03-14 00:49:35 +0500

    added projects page designs

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,55 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-5">
+        <form action="{{ route('projects.store') }}" method="POST">
+            @csrf
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Project</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body>
-    <div>
-        <div class="max-w-7xl mx-auto mt-5">
-            <form action="{{ route('projects.store') }}" method="POST">
-                @csrf
-
-                <div class="sm:overflow-hidden sm:rounded-md">
-                    <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
-                        <div>
-                            <h3 class="font-bold text-xl mb-2">{{ __('Create a Project') }}</h3>
-                            <p class="text-gray-500 text-xs">{{ __('Get start by creating a new project') }}</p>
-                        </div>
-                        <div class="grid grid-cols-3 gap-6">
-                            <div class="col-span-3 sm:col-span-2">
-                                <label for="title" class="block text-sm font-medium leading-6 text-gray-900">
-                                    {{ __('Title') }}
-                                </label>
-                                <div class="mt-2 flex rounded-md">
-                                    <input type="text" name="title" id="title" autocomplete="title" class="block w-full max-w-lg rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                                </div>
-                            </div>
-                        </div>
-
-                        <div>
-                            <label for="description" class="block text-sm font-medium leading-6 text-gray-900">{{__('Description')}}</label>
-                            <div class="mt-2">
-                                <textarea id="description" name="description" rows="3" class="mt-1 block w-full rounded-md border-0 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:py-1.5 sm:text-sm sm:leading-6" placeholder="Project Description"></textarea>
-                            </div>
-                            <p class="mt-2 text-sm text-gray-500">{{ __('Add brief description for your project') }}</p>
-                        </div>
-                        <button type="submit" class="inline-flex items-center gap-x-1.5 rounded-md bg-blue-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
-                            <svg class="-ml-0.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                            </svg>
-                            {{ __('Submit') }}
-                        </button>
+            <div class="sm:overflow-hidden sm:rounded-md">
+                <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
+                    <div>
+                        <h3 class="font-bold text-xl mb-2">{{ __('Create a Project') }}</h3>
+                        <p class="text-gray-500 text-xs">{{ __('Get start by creating a new project') }}</p>
                     </div>
-                </div>
-            </form>
-        </div>
-    </div>
-</body>
+                    <div class="grid grid-cols-3 gap-6">
+                        <div class="col-span-3 sm:col-span-2">
+                            <label for="title" class="block text-sm font-medium leading-6 text-gray-900">
+                                {{ __('Title') }}
+                            </label>
+                            <div class="mt-2 flex rounded-md">
+                                <input type="text" name="title" id="title" autocomplete="title" class="block w-full max-w-lg rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                            </div>
+                        </div>
+                    </div>
 
-</html>
+                    <div>
+                        <label for="description" class="block text-sm font-medium leading-6 text-gray-900">{{__('Description')}}</label>
+                        <div class="mt-2">
+                            <textarea id="description" name="description" rows="3" class="mt-1 block w-full rounded-md border-0 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:py-1.5 sm:text-sm sm:leading-6" placeholder="Project Description"></textarea>
+                        </div>
+                        <p class="mt-2 text-sm text-gray-500">{{ __('Add brief description for your project') }}</p>
+                    </div>
+                    <button type="submit" class="button">
+                        <svg class="-ml-0.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                        </svg>
+                        {{ __('Submit') }}
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</x-app-layout>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,26 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex items-end justify-between">
+            <h2 class="font-normal text-base text-gray-400 leading-tight">
+                {{ __('My Projects') }}
+            </h2>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Birdboard</title>
-</head>
-
-<body>
-    <div>
-        <h1>Birdboard</h1>
-        <ul>
+            <a href="{{ route('projects.create') }}" type="submit" class="button">
+                {{ __('Add Project') }}
+            </a>
+        </div>
+    </x-slot>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-5">
+        <div class="lg:flex lg:flex-wrap -mx-3">
             @forelse ($projects as $project)
-                <li>
-                    <a href="{{ $project->path() }}">{{ $project->title }}</a>
-                </li>
-            @empty
-                <li>No projects yet</li>
-            @endforelse
-        </ul>
-    </div>
-</body>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white rounded-lg shadow-sm h-[200px] p-5">
+                    <h3 class="font-bold text-xl py-4 border-l-4 border-blue-500 mb-3 -ml-5 pl-5">
+                        <a href="{{ $project->path() }}" class="text-black">{{ $project->title }}</a>
+                    </h3>
+                    <div class="text-gray-400">{{ \Illuminate\Support\Str::limit($project->description, 100) }}</div>
+                </div>
+            </div>
 
-</html>
+            @empty
+            <div class="text-center relative bg-white py-12 px-4 rounded">
+                <svg class="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path vector-effect="non-scaling-stroke" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
+                </svg>
+                <h3 class="mt-2 text-sm font-semibold text-gray-900">No projects</h3>
+                <p class="mt-1 text-sm text-gray-500">Get started by creating a new project.</p>
+                <div class="mt-6">
+                    <a href="{{ route('projects.create') }}" type="button" class="button">
+                        <svg class="-ml-0.5 mr-1.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                        </svg>
+                        {{ __('New Project') }}
+                    </a>
+                </div>
+            </div>
+            @endforelse
+        </div>
+    </div>
+</x-app-layout>
