011051c3b1711f7c3a16451890bcdac30600b17c
Soban Farooq <sobanfarooq3@gmail.com>
2023-05-29 16:09:07 +0500

    minor styling fixes

--- resources/views/projects/activity/card.blade.php
+++ resources/views/projects/activity/card.blade.php
@@ -1,8 +1,10 @@
 <div class="px-3 mt-3 card space-y-1.5">
     @foreach ($project->activity as $activity)
         <li class="flex items-center justify-between text-sm list-none">
-            @include("projects.activity.{$activity->description}")
-            <span class="text-xs text-gray-400">{{ $activity->created_at->diffForHumans(null, true) }}</span>
+            <span class="w-10/12">
+                @include("projects.activity.{$activity->description}")
+            </span>
+            <span class="flex-none text-xs text-gray-400">{{ $activity->created_at->diffForHumans(null, true) }}</span>
         </li>
     @endforeach
 </div>
